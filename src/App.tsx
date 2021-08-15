8946bc1ade19f912479f054a46419b278f133d9b
Nour-eddine Taleb <contact@noureddine.xyz>
2021-08-15 12:14:24 +0100

    add fontawesome icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import './App.css';
 import { NavBar } from './NavBar/NavBar';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fas);
 
 function App() {
   return (
