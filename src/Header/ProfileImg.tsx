b58993aea1482b94cbb8bdb6a5e6249cfdec441f
Nour-eddine Taleb <contact@noureddine.xyz>
2021-08-16 11:20:06 +0100

    header: create profileimg element

---
+++ src/Header/ProfileImg.tsx
@@ -0,0 +1,12 @@
+export function ProfileImg()
+{
+	return (
+		<div className="flex gap-2 items-center">
+			<div className="flex flex-col">
+				<span>User Name</span>
+				<span>Role</span>
+			</div>
+			<img className="w-10 h-10 rounded-full bg-white" src="ram.png" alt=""/>
+		</div>
+	);
+}
