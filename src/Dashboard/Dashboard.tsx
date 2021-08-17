a877b9336901f2210dcbef704acd2ee3a60c5eef
Nour-eddine Taleb <contact@noureddine.xyz>
2021-08-17 13:30:49 +0100

    dashboard: add statCard to the view

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,6 +1,18 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { StatCard } from "./StatCard";
+
 export function Dashboard()
 {
 	return (
-		<h1>Dashboard</h1>
+		<div className="relative">
+			<div className="flex justify-around relative top-[-30px]">
+				<StatCard title="Stagiaire" icon={<FontAwesomeIcon icon="user-graduate"/>}
+						count={92} scheme={{ bg: "bg-purple-500 bg-opacity-[10%]", text: "text-purple-800" }}></StatCard>
+				<StatCard title="Instructeur" icon={<FontAwesomeIcon icon="chalkboard-teacher"/>}
+						count={23} scheme={{ bg: "bg-green-500 bg-opacity-[10%]", text: "text-green-400" }}></StatCard>
+				<StatCard title="Formation" icon={<FontAwesomeIcon icon="tablet"/>}
+						count={33} scheme={{ bg: "bg-yellow-500 bg-opacity-[10%]", text: "text-yellow-400" }}></StatCard>
+			</div>
+		</div>
 	)
 }
