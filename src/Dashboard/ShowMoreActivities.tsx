afb02e07dd0121d8a3d027c81bc57b809b5b2c52
Nour-eddine Taleb <n.taleb@itroadgroup.com>
2021-08-19 14:15:24 +0100

    dashboard: create a ShowMoreActivities component

---
+++ src/Dashboard/ShowMoreActivities.tsx
@@ -0,0 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export function ShowMoreActivities()
+{
+	return (
+		<div className="text-[#B23939] flex items-center justify-center font-bold">
+			<h2>
+				More Activity 
+			</h2>
+			<FontAwesomeIcon icon="arrow-right" className="text-[#B23939] ml-1.5"></FontAwesomeIcon>
+		</div>
+	);
+}
