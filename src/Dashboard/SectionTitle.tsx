a8a31b8fb4f7766d8f03855535bb1a9a27cb8141
Nour-eddine Taleb <n.taleb@itroadgroup.com>
2021-08-19 14:16:00 +0100

    dashboard: add margin to the title

--- src/Dashboard/SectionTitle.tsx
+++ src/Dashboard/SectionTitle.tsx
@@ -1,6 +1,6 @@
 export function SectionTitle({title}: {title: string})
 {
 	return (
-		<h1 className="text-[#163072] text-lg font-bold uppercase mb-5">{title}</h1>
+		<h1 className="text-[#163072] text-lg font-bold uppercase mb-5 mt-6">{title}</h1>
 	);
 }
