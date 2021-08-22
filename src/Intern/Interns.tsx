4055214e63946acc6743dfd17a01dba0592a8762
Nour-eddine Taleb <n.taleb@itroadgroup.com>
2021-08-22 13:23:32 +0100

    intern: make button like divs clickable

--- src/Intern/Interns.tsx
+++ src/Intern/Interns.tsx
@@ -19,29 +19,29 @@ export function Interns() {
 	]
 
 	const data = [
-		["lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", <div className="flex justify-center items-center"> <div className="text-[#2AD4B9] border border-[#2AD4B9] rounded w-20 h-8 flex items-center justify-center font-bold">Validée</div></div>],
-		["lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", <div className="flex justify-center items-center"> <div className="text-[#2AD4B9] border border-[#2AD4B9] rounded w-20 h-8 flex items-center justify-center font-bold">Validée</div></div>],
-		["lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", <div className="flex justify-center items-center"> <div className="text-[#2AD4B9] border border-[#2AD4B9] rounded w-20 h-8 flex items-center justify-center font-bold">Validée</div></div>],
-		["lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", <div className="flex justify-center items-center"> <div className="text-[#2AD4B9] border border-[#2AD4B9] rounded w-20 h-8 flex items-center justify-center font-bold">Validée</div></div>],
-		["lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", <div className="flex justify-center items-center"> <div className="text-[#2AD4B9] border border-[#2AD4B9] rounded w-20 h-8 flex items-center justify-center font-bold">Validée</div></div>],
-		["lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", <div className="flex justify-center items-center"> <div className="text-[#2AD4B9] border border-[#2AD4B9] rounded w-20 h-8 flex items-center justify-center font-bold">Validée</div></div>],
-		["lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", <div className="flex justify-center items-center"> <div className="text-[#2AD4B9] border border-[#2AD4B9] rounded w-20 h-8 flex items-center justify-center font-bold">Validée</div></div>],
+		["lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", <div className="flex justify-center items-center"> <div className="cursor-pointer text-[#2AD4B9] border border-[#2AD4B9] rounded w-20 h-8 flex items-center justify-center font-bold">Validée</div></div>],
+		["lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", <div className="flex justify-center items-center"> <div className="cursor-pointer text-[#2AD4B9] border border-[#2AD4B9] rounded w-20 h-8 flex items-center justify-center font-bold">Validée</div></div>],
+		["lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", <div className="flex justify-center items-center"> <div className="cursor-pointer text-[#2AD4B9] border border-[#2AD4B9] rounded w-20 h-8 flex items-center justify-center font-bold">Validée</div></div>],
+		["lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", <div className="flex justify-center items-center"> <div className="cursor-pointer text-[#2AD4B9] border border-[#2AD4B9] rounded w-20 h-8 flex items-center justify-center font-bold">Validée</div></div>],
+		["lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", <div className="flex justify-center items-center"> <div className="cursor-pointer text-[#2AD4B9] border border-[#2AD4B9] rounded w-20 h-8 flex items-center justify-center font-bold">Validée</div></div>],
+		["lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", <div className="flex justify-center items-center"> <div className="cursor-pointer text-[#2AD4B9] border border-[#2AD4B9] rounded w-20 h-8 flex items-center justify-center font-bold">Validée</div></div>],
+		["lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", <div className="flex justify-center items-center"> <div className="cursor-pointer text-[#2AD4B9] border border-[#2AD4B9] rounded w-20 h-8 flex items-center justify-center font-bold">Validée</div></div>],
 	]
 
 	return (
 		<div>
 			<div className="rounded-lg overflow-hidden">
 				<div className="w-full h-24 bg-white flex items-center justify-between px-3">
-					<div className="bg-[#2AD4B9] flex rounded-lg w-72 h-11 items-center justify-center px-3 text-white font-bold text-base">
+					<div className="cursor-pointer bg-[#2AD4B9] flex rounded-lg w-72 h-11 items-center justify-center px-3 text-white font-bold text-base">
 						<IconPlus className="w-7 h-7 mr-9" />
 						Ajouter un Stagiaire
 					</div>
 					<div className="flex gap-1">
 						<input className="hover:outline-none border border-[#929292] border-opacity-50 appearance-none rounded pl-5 placeholder-[#929292]" placeholder="Recherche" type="text" />
-						<div className="bg-[#EC7C89] w-11 h-11 text-white rounded flex justify-center items-center">
+						<div className="cursor-pointer bg-[#EC7C89] w-11 h-11 text-white rounded flex justify-center items-center">
 							<IconPreference className="w-6 h-6" ></IconPreference>
 						</div>
-						<div className="bg-[#C40B33] w-11 h-11 text-white rounded flex justify-center items-center">
+						<div className="cursor-pointer bg-[#C40B33] w-11 h-11 text-white rounded flex justify-center items-center">
 							<IconSearch className="w-6 h-6" ></IconSearch>
 						</div>
 					</div>
@@ -65,10 +65,10 @@ export function Interns() {
 										{r.map(d => <td>{d}</td>)}
 										{
 											<td className="flex justify-center my-3 gap-1">
-												<div className="w-[28px] h-[28px] bg-[#2AD4B9] text-white rounded-md flex items-center justify-center px-1" >
+												<div className="cursor-pointer w-[28px] h-[28px] bg-[#2AD4B9] text-white rounded-md flex items-center justify-center px-1" >
 													<IconEye />
 												</div>
-												<div className="w-[28px] h-[28px] bg-[#FDBC64] text-white rounded-md flex items-center justify-center px-1" >
+												<div className="cursor-pointer w-[28px] h-[28px] bg-[#FDBC64] text-white rounded-md flex items-center justify-center px-1" >
 													<IconPencil className="w-[13px] h-[13px]" />
 												</div>
 											</td>
