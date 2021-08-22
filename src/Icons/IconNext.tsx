69cbdb3b8e6f08e76d5f4ae8ae1dbe774a496f48
Nour-eddine Taleb <n.taleb@itroadgroup.com>
2021-08-22 13:15:07 +0100

    app: add extra icons

---
+++ src/Icons/IconNext.tsx
@@ -0,0 +1,9 @@
+export function IconNext({className}: {className?: string})
+{
+	return (
+		<svg className={className} viewBox="0 0 19 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M9.60046 10.4055L13.9544 6.8171L9.60046 3.22873" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+		<path d="M4.52087 10.4055L8.87476 6.8171L4.52087 3.22873" opacity=".5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+		</svg>
+	);
+}
