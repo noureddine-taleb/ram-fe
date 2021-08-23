13e951cdcaadc262f8fc008f414dbeb6a89a46a5
Nour-eddine Taleb <n.taleb@itroadgroup.com>
2021-08-24 00:10:13 +0100

    Intern: create new component UploadImg

---
+++ src/Intern/UploadImg.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { IconImg } from "../Icons/IconImg";
+
+export function UploadImg() {
+	return (
+		<button className="w-1/6 h-72 bg-[#FFE2E2] text-[#EF9797] rounded-lg flex flex-col items-center justify-center">
+			<IconImg className="w-28 h-28"></IconImg>
+			<div className="font-bold text-lg">Charger une Photo</div>
+		</button>
+	);
+}
