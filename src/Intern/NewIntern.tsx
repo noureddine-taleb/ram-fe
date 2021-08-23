b3ed31909c1efe002fbfc117ae30a5baafecf9d6
Nour-eddine Taleb <n.taleb@itroadgroup.com>
2021-08-24 00:11:20 +0100

    NewIntern: create the main view

--- src/Intern/NewIntern.tsx
+++ src/Intern/NewIntern.tsx
@@ -1,6 +1,20 @@
+import { Header } from "./Header";
+import { PersonalInfoUpper } from "./PersonalInfoUpper";
+import { PersonalInfoMiddle } from "./PersonalInfoMiddle";
+import { PersonalInfoLower } from "./PersonalInfoLower";
+import { Button } from "./Button";
+
 export function NewIntern()
 {
 	return (
-		<h1>Ajouter Stagiaire</h1>
+		<div className="flex flex-col">
+			<div className="rounded-lg px-2">
+				<Header title="Information Personnelles"/>
+				<PersonalInfoUpper></PersonalInfoUpper>
+				<PersonalInfoMiddle></PersonalInfoMiddle>
+				<PersonalInfoLower></PersonalInfoLower>
+				<Button className="w-full h-14 bg-[#D7FCF6] text-[#2AD4B9] border border-[#2AE7C8]" label="Enregister"></Button>
+			</div>
+		</div>
 	)
 }
