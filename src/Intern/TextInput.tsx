c88a8b1fb971ca644afdea79e5777c1b013236be
Nour-eddine Taleb <n.taleb@itroadgroup.com>
2021-08-24 00:10:18 +0100

    Intern: create new component TextInput

---
+++ src/Intern/TextInput.tsx
@@ -0,0 +1,8 @@
+export function TextInput({name, id, label, placeholder, className}: {name: string, id: string, label: string, placeholder: string, className?: string}) {
+	return (
+		<div className={`flex flex-col ${className}`}>
+			<label className="text-[#525F7F] font-bold text-sm pb-1" htmlFor={name}>{label}</label>
+			<input className="pl-2 placeholder-gray-400 hover:outline-none shadow-md rounded appearance-none h-8 text-xs" type="text" id={id} name={name} placeholder={placeholder} />
+		</div>
+	);
+}
