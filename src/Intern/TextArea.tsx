247b4b4138c809afe0febf745dfc33d557bfcd81
Nour-eddine Taleb <n.taleb@itroadgroup.com>
2021-08-24 00:10:23 +0100

    Intern: create new component TextArea

---
+++ src/Intern/TextArea.tsx
@@ -0,0 +1,8 @@
+export function TextArea({name, id, label, placeholder, className}: {name: string, id: string, label: string, placeholder: string, className?: string}) {
+	return (
+		<div className={`flex flex-col ${className}`}>
+			<label className="text-[#525F7F] font-bold text-sm pb-1" htmlFor={name}>{label}</label>
+			<textarea className="pl-2 pt-2 h-full placeholder-gray-400 hover:outline-none shadow-md rounded appearance-none text-xs" id={id} name={name} placeholder={placeholder} />
+		</div>
+	);
+}
