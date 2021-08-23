ccf2660f11a86970c3d89522f9400c46d298f93e
Nour-eddine Taleb <n.taleb@itroadgroup.com>
2021-08-24 00:09:43 +0100

    Intern: create new component CheckboxInput

---
+++ src/Intern/CheckboxInput.tsx
@@ -0,0 +1,19 @@
+export function CheckboxInput({name, ids, label, values, className}: {name: string, ids: string[], label: string, values: string[], className?: string}) {
+	return (
+		<div className={`flex flex-col ${className}`}>
+			<label className="text-[#525F7F] font-bold text-sm pb-1" htmlFor={name}>{label}</label>
+			<div className="flex gap-2">
+				{
+					values.map((v, i) => {
+						return (
+							<div className="flex pl-2 shadow-md rounded appearance-none h-8 bg-white items-center px-5 gap-2 w-1/2">
+								<input type="radio" name={name} value={v} id={ids[i]}></input>
+								<label className="text-xs" htmlFor={ids[i]}>{v}</label>
+							</div>
+						)
+					})
+				}
+			</div>
+		</div>
+	);
+}
