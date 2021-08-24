534e88e8ba57bf464b2e1f4689838c76a895d9f0
Nour-eddine Taleb <n.taleb@itroadgroup.com>
2021-08-24 16:19:50 +0100

    Intern: trigger file upload input when click on UploadImg

--- src/views/Intern/components/UploadImg.tsx
+++ src/views/Intern/components/UploadImg.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useRef } from "react";
 import { IconImg } from "../../../components/Icons/IconImg";
 
 export function UploadImg() {
+	const inputRef = useRef<HTMLInputElement>(null);
+
 	return (
-		<button className="w-1/6 h-72 bg-[#FFE2E2] text-[#EF9797] rounded-lg flex flex-col items-center justify-center">
+		<button onClick={() => inputRef?.current?.click() } className="w-1/6 h-72 bg-[#FFE2E2] text-[#EF9797] rounded-lg flex flex-col items-center justify-center">
 			<IconImg className="w-28 h-28"></IconImg>
 			<div className="font-bold text-lg">Charger une Photo</div>
+			<input className="hidden" ref={inputRef} type="file" name="" id=""/>
 		</button>
 	);
 }
