e62c0b52365f17b656cf13b360f1b4c8c20d8835
Nour-eddine Taleb <n.taleb@itroadgroup.com>
2021-08-30 14:53:24 +0100

    training: setup routing

--- src/views/Training/routes.tsx
+++ src/views/Training/routes.tsx
@@ -1,7 +1,13 @@
+import { Cours } from "./Cours";
+import { CoursEval } from "./CoursEval";
+import { CoursPlan } from "./CoursPlan";
 import { NewTraining } from "./NewTraining";
 import { Trainings } from "./Trainings";
 
 export const TrainingRoutes = [
 	{ path: "/", element: <Trainings /> },
+	{ path: "/cours", element: <Cours /> },
+	{ path: "/cours/planification-cours", element: <CoursPlan /> },
+	{ path: "/cours/notation", element: <CoursEval /> },
 	{ path: "ajouter-formation", element: <NewTraining /> }
 ];
