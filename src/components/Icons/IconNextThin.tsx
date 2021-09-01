2c2af8aa0133633fe95c4381c87a292f9645edcd
Nour-eddine Taleb <n.taleb@itroadgroup.com>
2021-09-01 22:25:49 +0100

    app: create extra icons

---
+++ src/components/Icons/IconNextThin.tsx
@@ -0,0 +1,8 @@
+export function IconNextThin({ className }: { className?: string }) {
+	return (
+		<svg className={className} viewBox="0 0 40 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M12 43L37 23L12 3" stroke="currentColor" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" />
+			<path d="M3 39L21 23L3 7" stroke="currentColor" stroke-opacity="0.5" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" />
+		</svg>
+	);
+}
