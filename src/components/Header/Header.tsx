a47c73b3edb3d3fe386b8e5dbce0d5faa0af1776
Nour-eddine Taleb <n.taleb@itroadgroup.com>
2021-10-21 22:02:03 +0100

    header: align title with tool-bar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,19 +7,22 @@ import { MobileMenu } from "./components/MobileMenu";
 import { NavElement } from "interfaces/NavElement";
 
 export function Header({ list }: { list: NavElement[] }) {
-	const currentLoc = useLocation().pathname;
-	return (
-		<header className="bg-red w-full lg:w-4/5 h-[25%] bg-gradient-to-r from-[#f0405c] to-[#c20831] text-white flex flex-col pt-5 relative">
-			<MobileMenu list={list} className="top-[72px] lg:hidden" />
-			<div className="ml-4 font-sans text-lg">
-				{getHeader(currentLoc)}
-			</div>
-			<div className="ml-auto flex items-center gap-4 sm:gap-8 mr-4 mt-4">
-				<SearchBar />
-				<IconBell className="mt-1 w-6 h-7" notificationAvailable={true}></IconBell>
-				<span>|</span>
-				<ProfileImg />
-			</div>
-		</header>
-	);
+  const currentLoc = useLocation().pathname;
+  return (
+    <header className="bg-red w-full lg:w-4/5 h-[25%] bg-gradient-to-r from-[#f0405c] to-[#c20831] text-white flex flex-col pt-5 relative">
+      <MobileMenu list={list} className="top-[72px] lg:hidden" />
+      <div className="flex items-center mt-10">
+        <div className="ml-4 font-sans text-lg">{getHeader(currentLoc)}</div>
+        <div className="ml-auto flex items-center gap-4 sm:gap-8 mr-4">
+          <SearchBar />
+          <IconBell
+            className="mt-1 w-6 h-7"
+            notificationAvailable={true}
+          ></IconBell>
+          <span>|</span>
+          <ProfileImg />
+        </div>
+      </div>
+    </header>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 module.exports = {
-	mode: "jit",
-	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-	textColor: {
-		red: "#c20831",
-		green: "#2ad4b9",
-		yellow: "#fdbc64",
-		blue: "#648cb2",
-	}
-}
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  textColor: {
+    red: "#c20831",
+    green: "#2ad4b9",
+    yellow: "#fdbc64",
+    blue: "#648cb2",
+  },
+  theme: {
+    extend: {
+      colors: {
+        "ram-red": "#c3022d",
+      },
+    },
+  },
+};
